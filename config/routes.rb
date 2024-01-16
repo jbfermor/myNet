fc30be45b8d73d8c383e62baa274560519867fc4
jbfermor <jbfermor@hotmail.com>
2024-01-16 15:53:15 +0100

    minor fixes to add sign up button

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   resources :comments
   resources :posts
   resources :bios
+  get "/", to: "home#index"
   devise_for :users
-  get 'home/index'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
